fafe639c112a675aa4afafb1fcc93d0ad4d007eb
Peter Chen <peterchen2017125@gmail.com>
2025-01-24 13:45:44 +0800

    feat: add client-side rendering support to Providers component

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,19 +1,26 @@
 "use client";
 
+import * as React from "react";
 import { ThemeProvider } from "./theme-provider";
 
 function Providers({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
   return (
-    <>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
   );
 }
 
