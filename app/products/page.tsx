5a40b375888b87ee0a429e206904e21382a84511
Peter Chen <peterchen2017125@gmail.com>
2025-01-27 11:20:34 +0800

    feat: implement ProductsContainer and ProductsList components for product display with layout options

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,13 @@
-function ProductPages() {
-  return <div>ProductPages</div>;
+import ProductsContainer from "@/components/product/ProductsContainer";
+
+function ProductPages({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) {
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+  return <ProductsContainer layout={layout} search={search} />;
 }
 
 export default ProductPages;
--- components/product/ProductsContainer.tsx
+++ components/product/ProductsContainer.tsx
@@ -1,5 +1,65 @@
-function ProductsContainer() {
-  return <div>ProductsContainer</div>;
+import { fetchAllProducts } from "@/utils/actions";
+import Link from "next/link";
+import { LuLayoutGrid, LuLayoutList } from "react-icons/lu";
+import { Button } from "../ui/button";
+import ProductsGrid from "./ProductsGrid";
+import ProductsList from "./ProductsList";
+import { Separator } from "@/components/ui/separator";
+
+async function ProductsContainer({
+  layout,
+  search,
+}: {
+  layout: string;
+  search: string;
+}) {
+  const products = await fetchAllProducts();
+  const totalProducts = products.length;
+  const searchTerm = search ? `&search=${search}` : "";
+  return (
+    <>
+      <section>
+        <div className="flex justify-between items-center">
+          <h4 className="font-medium text-lg">
+            {totalProducts} Product{totalProducts > 1 && "s"}
+          </h4>
+          <div className="flex gap-x-4">
+            <Button
+              variant={layout === "grid" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/products?layout=grid${searchTerm}`}>
+                <LuLayoutGrid />
+              </Link>
+            </Button>
+            <Button
+              variant={layout === "list" ? "default" : "ghost"}
+              size="icon"
+              asChild
+            >
+              <Link href={`/products?layout=list${searchTerm}`}>
+                <LuLayoutList />
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <Separator className="my-4" />
+      </section>
+      <div>
+        {totalProducts === 0 ? (
+          <h5 className="text-2xl mt-16">
+            {" "}
+            Sorry no products matched your search ...
+          </h5>
+        ) : layout === "grid" ? (
+          <ProductsGrid products={products} />
+        ) : (
+          <ProductsList products={products} />
+        )}
+      </div>
+    </>
+  );
 }
 
 export default ProductsContainer;
