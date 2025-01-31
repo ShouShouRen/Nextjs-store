9909eacdc836497e972663964c5e416b20abe02f
Peter Chen <peterchen2017125@gmail.com>
2025-01-31 13:30:07 +0800

    feat: add Toaster component to Providers for improved user notifications

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 function Providers({ children }: { children: React.ReactNode }) {
   const [isMounted, setIsMounted] = React.useState(false);
@@ -13,14 +14,17 @@ function Providers({ children }: { children: React.ReactNode }) {
   if (!isMounted) return null;
 
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <>
+      <Toaster />
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 
