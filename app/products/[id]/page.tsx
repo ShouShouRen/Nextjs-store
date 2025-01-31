abc677db8a38588a32889346d7a919f7c3509b04
Peter Chen <peterchen2017125@gmail.com>
2025-01-31 11:45:19 +0800

    feat: single products page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,44 @@
+import BreadCrumbs from "@/components/single-product/BreadCrumbs";
+import { fetchSingleProduct } from "@/utils/actions";
+import Image from "next/image";
+import { formatCurrency } from "@/utils/format";
+import AddToCart from "@/components/single-product/AddToCart";
+import ProductRating from "@/components/single-product/ProductRating";
+import FavoriteToggleButton from "@/components/product/FavoriteToggleButton";
+async function SingleProductPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  const product = await fetchSingleProduct(id);
+  const { name, image, company, description, price } = product;
+  const dollarsAmount = formatCurrency(price);
+  return (
+    <section>
+      <BreadCrumbs name={product.name} />
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        <div className="relative h-full">
+          <Image
+            src={image}
+            alt={name}
+            layout="fill"
+            objectFit="cover"
+            sizes="(max-width:768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        </div>
+        <div>
+          <div className="flex gap-x-8 items-center">
+            <h1 className="capitalize text-3xl font-bold">{name}</h1>
+            <FavoriteToggleButton productId={id} />
+          </div>
+          <ProductRating productId={id} />
+          <h4 className="text-xl mt-2">{company}</h4>
+          <p className="mt-3 text-md bg-muted inline-block p-2 rounded">
+            {dollarsAmount}
+          </p>
+          <p className="mt-6 leading-8 text-muted-foreground">{description}</p>
+          <AddToCart productId={id} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default SingleProductPage;
