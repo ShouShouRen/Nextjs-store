6e3a7abbb18ecf000bf1ba59546cad1517dfddb9
Peter Chen <peterchen2017125@gmail.com>
2025-01-31 18:28:44 +0800

    feat: enhance navbar with user authentication and profile features

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -7,19 +7,25 @@ import Logo from "./Logo";
 import NavSearch from "./NavSearch";
 function Navbar() {
   return (
-    <nav className="border-b">
-      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
-        <Logo />
-        <Suspense>
-          <NavSearch />
-        </Suspense>
-        <div className="flex gap-4 items-center">
-          <CartButton />
-          <DarkMode />
-          <LinksDropdown />
-        </div>
-      </Container>
-    </nav>
+    <>
+      <span className="h-12 inline-block flex items-center justify-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        accusamus cumque voluptates voluptatibus
+      </span>
+      <nav className="border-b shadow-lg w-full sticky top-0 z-50 bg-white dark:bg-gray-900">
+        <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
+          <Logo />
+          <Suspense>
+            <NavSearch />
+          </Suspense>
+          <div className="flex gap-4 items-center">
+            <CartButton />
+            <DarkMode />
+            <LinksDropdown />
+          </div>
+        </Container>
+      </nav>
+    </>
   );
 }
 
