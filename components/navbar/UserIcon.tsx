4c49828db2ae5ad4ffa1bdc8709f14bda35356f1
Peter Chen <peterchen2017125@gmail.com>
2025-02-05 12:58:09 +0800

    feat: replace standard img with Next.js Image component for user profile display

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,5 +1,6 @@
 import { LuUser } from "react-icons/lu";
 import { currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
 
 async function UserIcon() {
   const user = await currentUser();
@@ -8,7 +9,7 @@ async function UserIcon() {
 
   if (profileImage) {
     return (
-      <img
+      <Image
         src={profileImage}
         alt="User Profile"
         className="w-6 h-6 rounded-full object-cover"
