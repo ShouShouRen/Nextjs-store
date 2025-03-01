95be75bc2555f87e811a956f08fc73f8536defcd
Peter Chen <peterchen2017125@gmail.com>
2025-03-01 21:27:10 +0800

    feat: integrate Ads component into Navbar for enhanced visibility

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from "react";
 import Container from "../global/Container";
+import Ads from "./Ads";
 import CartButton from "./CartButton";
 import DarkMode from "./DarkMode";
 import LinksDropdown from "./LinksDropdown";
@@ -8,10 +9,7 @@ import NavSearch from "./NavSearch";
 function Navbar() {
   return (
     <>
-      <span className="h-12 inline-block flex items-center justify-center">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
-        accusamus cumque voluptates voluptatibus
-      </span>
+      <Ads />
       <nav className="border-b shadow-lg w-full sticky top-0 z-50 bg-white dark:bg-gray-900">
         <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
           <Logo />
